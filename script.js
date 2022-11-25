5c2d8f7473aed24d8f458683637bb88af2148e39
Celdor132 <noah.a.malikmelton@gmail.com>
2022-11-25 13:58:46 -0700

    Store broken function,experiment with scoring func

--- script.js
+++ script.js
@@ -6,59 +6,63 @@ let computerScore = 0;
 const choices = ["rock", "paper", "scissors"]; //choices for the computer
 function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random()* 3)]
+    console.log(computerChoice);
     return(computerChoice);
 }
-function playRound(playerSelection, computerSelection) {
+function declareWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return("TIE! Your weapons collapsed the universe.");
+        return "tie";
         //player chooses rock
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            return("You crushed their scissors with your rock!");
-            playerScore +=1;
+            return "win";
         }
         if (computerSelection === "paper") {
-            return("Your rock was smothered by their paper...");
-            computerScore +=1;
+            return "lose";
         }
         //player chooses paper
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return("Your paper smothered their rock!!!!");
-            playerScore +=1;
+            return "win";
         }
-        if (computerSelection === "scissors") {
-            return("Your paper was shredded by their scissors...");
-            computerScore +=1;
-        }
+        if (computerSelection === "scissors") 
+            return "lose";
         //player chooses scissors
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            return("Your scissors shredded their paper!!");
-            playerScore +=1;
+            return "win";
         }
         if (computerSelection === "rock") {
-            return("Your scissors were crushed by their rock...");
-            computerScore += 1;
+            return "lose";
         }
         //player chooses gun
     } else if (playerSelection === "gun") {
-        return("Thats cheating but... you win...");
-        playerScore +=1;
+        return "cheat";
         //player doesn't choose from the options
     } else {
         return(`"${playerSelection}" is not an authorized weapon. Try again!`)
     }
 }
+function updateScore() {
+    if (declareWinner() == "tie") {
+        return("No winners in this battle.");
+    } else if (declareWinner() = "win") {
+        return("You crushed your enemy.");
+    } else {
+        return("You are dead.");
+    }
+}
+
+
 
 function game() {
     for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
+    declareWinner(playerSelection, computerSelection);
     console.log(`You have won ${playerScore} times and you have lost ${computerScore} times.`)
     }
 }
 const playerSelection = prompt("Choose rock, paper, or scissors.").toLowerCase();
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
-game();
+console.log(updateScore());
+//game();
