6622bd65ab159aefb1bde41f318530cd0560dcf4
Celdor132 <noah.a.malikmelton@gmail.com>
2022-11-26 13:02:00 -0700

    edit playerSelection, add checkRound

--- script.js
+++ script.js
@@ -1,68 +1,97 @@
 console.log("Hello there, welcome to the game!");
 
+rounds = 0;
 let playerScore = 0;
 let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];  //choices for the computer
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
 
-const choices = ["rock", "paper", "scissors"]; //choices for the computer
 function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random()* 3)]
-    console.log(computerChoice);
+    console.log(`The computer chose ${computerChoice}`);
     return(computerChoice);
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+    console.log(`The player chose ${playerChoice}`);
+    return(playerChoice);
+}
+
 function declareWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "tie";
+        return("tie");
+
         //player chooses rock
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            return "win";
+            return("win");
         }
         if (computerSelection === "paper") {
-            return "lose";
+            return("lose");
         }
+
         //player chooses paper
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return "win";
+            return("win");
         }
         if (computerSelection === "scissors") 
-            return "lose";
+            return("lose");
+
         //player chooses scissors
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            return "win";
+            return("win");
         }
         if (computerSelection === "rock") {
-            return "lose";
+            return("lose");
         }
+
         //player chooses gun
     } else if (playerSelection === "gun") {
-        return "cheat";
+        return("cheat");
+
         //player doesn't choose from the options
+    } else {
+        return("Unspecified error. Please figure it out.");
+    }
+}
+
+function updateScore() {
+    if (declareWinner(playerSelection, computerSelection) === ("win")) {
+        playerScore++;
+        rounds++;
+        return("You win!");
+    } else if (declareWinner(playerSelection, computerSelection) === ("lose")) {
+        computerScore++;
+        rounds++;
+        return("You lose!");
+    } else if (declareWinner(playerSelection, computerSelection) === ("cheat")) {
+        playerScore++;
+        rounds++;
+        return("You cheated! But... you won.");
+    } else if (declareWinner(playerSelection, computerSelection) === ("tie")) {
+        console.log("No winners here");
     } else {
         return(`"${playerSelection}" is not an authorized weapon. Try again!`)
     }
 }
-function updateScore() {
-    if (declareWinner() == "tie") {
-        return("No winners in this battle.");
-    } else if (declareWinner() = "win") {
-        return("You crushed your enemy.");
-    } else {
-        return("You are dead.");
+
+function checkRounds(rounds) {
+    if (rounds < 5) {
+
     }
 }
-
-
 
 function game() {
-    for (let i = 0; i < 5; i++) {
     declareWinner(playerSelection, computerSelection);
-    console.log(`You have won ${playerScore} times and you have lost ${computerScore} times.`)
-    }
+    console.log(updateScore());
+    console.log(`Your score is ${playerScore} and the enemy's score is ${computerScore}. 
+    You have ${5 - rounds} rounds remaining.`)
+    
 }
-const playerSelection = prompt("Choose rock, paper, or scissors.").toLowerCase();
-const computerSelection = getComputerChoice();
 
-console.log(updateScore());
-//game();
+game();
+
